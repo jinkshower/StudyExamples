222a0e22d856c4ff4f76d343bbbad55b91be9023
jinkshower <135244018+jinkshower@users.noreply.github.com>
2023-09-19 17:54:01 +0900

    java first

---
+++ Java/Car.java
@@ -0,0 +1,10 @@
+public class Car {
+    String name;
+    int number;
+    public void forward() {
+        System.out.println("forward");
+    }
+    public void method2(int x) {
+        System.out.println(x);
+    }
+}
---
+++ Java/Sample.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Sample {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        Car c2 = new Car();
+
+        c1.name = "happy";
+        c1.forward();
+        c1.method2(2);
+    }
+}
