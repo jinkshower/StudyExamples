4eba7692064ccaa60bae06f952799f883e2ec50d
jinkshower <135244018+jinkshower@users.noreply.github.com>
2023-10-05 23:15:06 +0900

    Add files via upload

---
+++ Java/StackPractice11_3.java
@@ -0,0 +1,32 @@
+//import java.util.EmptyStackException;
+//import java.util.Stack;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        Stack st = new Stack();
+//        String expression = "((3+2) * 8))))";
+//
+//        System.out.println("expression : " + expression);
+//
+//        try {
+//            for (int i = 0; i < expression.length(); i++) {
+//                char ch = expression.charAt(i);
+//                if (ch == '(') {
+//                    st.push(ch);
+//                }
+//                if (ch == ')') {
+//                    st.pop();
+//                }
+//            }
+//            if (st.isEmpty()) {
+//                System.out.println("Bracket match!");
+//            }
+//            if (!st.isEmpty()) {
+//                System.out.println("Bracket Unmatch!");
+//            }
+//        } catch (EmptyStackException e){
+//            System.out.println("Bracket Unmatch!2222");
+//        }
+//    }
+//}
+//
